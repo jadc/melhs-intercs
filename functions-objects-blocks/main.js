3d99a86bdf94ee150a56efbbe3f4420cb28d5d03
Jad Chehimi <j.chehimi@EDU.epsb.ca>
2019-10-16 15:16:11 -0600

    FINISHED!

--- functions-objects-blocks/main.js
+++ functions-objects-blocks/main.js
@@ -7,26 +7,32 @@ cnv.width = 800;
 cnv.height = 600;
 
 // Global Variables
-let block1X = 100;
-let block1Y = 200;
-let block1W = 100;
-let block1H = 10
-let block1Xspeed = randomDec(-5, 5);
-let block1Color = 'orange';
+let block1 = {
+    x: 100,
+    y: 200,
+    w: 100,
+    h: 10,
+    speed: randomDec(-5, 5),
+    color: "orange"
+}
 
-let block2X = 300;
-let block2Y = 300;
-let block2W = 75;
-let block2H = 10
-let block2Xspeed = randomDec(-5, 5);
-let block2Color = 'purple';
+let block2 = {
+    x: 300,
+    y: 300,
+    w: 75,
+    h: 10,
+    speed: randomDec(-5, 5),
+    color: "purple"
+}
 
-let block3X = 500;
-let block3Y = 500;
-let block3W = 120;
-let block3H = 10
-let block3Xspeed = randomDec(-5, 5);
-let block3Color = 'green';
+let block3 = {
+    x: 500,
+    y: 500,
+    w: 120,
+    h: 10,
+    speed: randomDec(-5, 5),
+    color: "green"
+}
 
 // Animation Loop
 requestAnimationFrame(animate);
@@ -35,21 +41,21 @@ function animate() {
     // UPDATE
 
     // Update block 1 - move and bounce horizontally
-    block1X += block1Xspeed;
-    if (block1X + block1W > cnv.width || block1X < 0) {
-        block1Xspeed = -block1Xspeed;
+    block1.x += block1.speed;
+    if (block1.x + block1.w > cnv.width || block1.x < 0) {
+        block1.speed = -block1.speed;
     }
 
     // Update block 2 - move and bounce horizontally
-    block2X += block2Xspeed;
-    if (block2X + block2W > cnv.width || block2X < 0) {
-        block2Xspeed = -block2Xspeed;
+    block2.x += block2.speed;
+    if (block2.x + block2.w > cnv.width || block2.x < 0) {
+        block2.speed = -block2.speed;
     }
 
     // Update block 3 - move and bounce horizontally
-    block3X += block3Xspeed;
-    if (block3X + block3W > cnv.width || block3X < 0) {
-        block3Xspeed = -block3Xspeed;
+    block3.x += block3.speed;
+    if (block3.x + block3.w > cnv.width || block3.x < 0) {
+        block3.speed = -block3.speed;
     }
 
     // DRAW
@@ -58,16 +64,16 @@ function animate() {
     ctx.clearRect(0, 0, cnv.width, cnv.height);
 
     // Draw block 1
-    ctx.fillStyle = block1Color;
-    ctx.fillRect(block1X, block1Y, block1W, block1H);
+    ctx.fillStyle = block1.color;
+    ctx.fillRect(block1.x, block1.y, block1.w, block1.h);
 
     // Draw block 2
-    ctx.fillStyle = block2Color;
-    ctx.fillRect(block2X, block2Y, block2W, block2H);
+    ctx.fillStyle = block2.color;
+    ctx.fillRect(block2.x, block2.y, block2.w, block2.h);
 
     // Draw block 3
-    ctx.fillStyle = block3Color;
-    ctx.fillRect(block3X, block3Y, block3W, block3H);
+    ctx.fillStyle = block3.color;
+    ctx.fillRect(block3.x, block3.y, block3.w, block3.h);
 
     // Request Another Animation Frame
     requestAnimationFrame(animate);
--- line-analyzer/main.js
+++ line-analyzer/main.js
@@ -13,11 +13,11 @@ function analyzeLine() {
 
     // Call Analyze Functions and Display results
     document.getElementById('length').innerHTML = getLength(pt1x, pt1y, pt2x, pt2y);
-    // document.getElementById('slope').innerHTML = getSlope(pt1x, pt1y, pt2x, pt2y);
-    // document.getElementById('description').innerHTML = getDescription(pt1x, pt1y, pt2x, pt2y);
-    // document.getElementById('location-1').innerHTML = getPointLocation(pt1x, pt1y);
-    // document.getElementById('location-2').innerHTML = getPointLocation(pt2x, pt2y);
-    // document.getElementById('equation').innerHTML = getEquation(pt1x, pt1y, pt2x, pt2y);
+    document.getElementById('slope').innerHTML = getSlope(pt1x, pt1y, pt2x, pt2y);
+    document.getElementById('description').innerHTML = getDescription(pt1x, pt1y, pt2x, pt2y);
+    document.getElementById('location-1').innerHTML = getPointLocation(pt1x, pt1y);
+    document.getElementById('location-2').innerHTML = getPointLocation(pt2x, pt2y);
+    document.getElementById('equation').innerHTML = getEquation(pt1x, pt1y, pt2x, pt2y);
 }
 
 // Line Analyzer Functions (Write your solutions here... getLength is done for you)
@@ -29,5 +29,48 @@ function getLength(x1, y1, x2, y2) {
     return (rise ** 2 + run ** 2) ** 0.5
 }
 
+function getSlope(x1, y1, x2, y2){
+    let rise = y2 - y1;
+    let run = x2 - x1;
+    if(run == 0) return undefined;
+    return (rise / run);
+}
 
+function getDescription(x1, y1, x2, y2){
+    let slope = getSlope(x1, y1, x2, y2);
+    if(slope > 0) return "increasing";
+    if(slope < 0) return "decreasing";
+    if(slope == 0) return "horizontal";
+    if(slope == undefined) return "vertical";
+}
 
+function getPointLocation(x, y){
+    if(x == 0 && y == 0) return "origin";
+    if(x == 0) return "y-axis";
+    if(y == 0) return "x-axis";
+    if(x > 0 && y > 0) return "quadrant 1";
+    if(x < 0 && y > 0) return "quadrant 2";
+    if(x < 0 && y < 0) return "quadrant 3";
+    if(x > 0 && y < 0) return "quadrant 4";
+}
+
+function getEquation(x1, y1, x2, y2){
+    let m = getSlope(x1, y1, x2, y2);
+    let b = y1 - m * x1;
+
+    // Horizontal Lines
+    if(m == 0) return `y = ${y1}`;
+    // Vertical Lines
+    if(m == undefined) return `x = ${x1}`;
+
+    // Normal Slopes
+    /*
+    Utilizes ternary operators to make 
+    the code unreadable-- I mean to hide unnecessary
+    numbers such as slope when it equals 1 or the y intercept when its 0
+    */
+    return "y = "
+     + ((m != 1) ? m : "") + "x"
+     + ((b != 0) ? (b > 0 ? " + " : " - ") : "")
+     + ((b != 0) ? " " + Math.abs(b) : "")
+}
