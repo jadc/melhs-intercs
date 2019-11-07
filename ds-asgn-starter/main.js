cd97166d3a1d2a326b1fb7f3943ea617d903111b
Jad Chehimi <j.chehimi@EDU.epsb.ca>
2019-11-07 15:30:53 -0700

    Mom

--- ds-asgn-starter/main.js
+++ ds-asgn-starter/main.js
@@ -1,21 +1,115 @@
-// DS ASGN STARTER TEMPLATE
+// Get data
+let map = {};
+(_ => {
+    // Get raw data into array
+    fetch("grocery-data.txt")
+    .then(r => { return r.text() })
+    .then(d => { return d.split("\r\n") })
+    .then(a => {
+        // Create map with array
+        for(let i = 0; i < a.length; i++){
+            let productName = a[i].split(";").shift();
+            let priceAndLocation = a[i].split(";").slice(1);
+            map[productName] = {
+                "price": priceAndLocation[0],
+                "location": priceAndLocation[1]
+            }
+        }
+        // This map has keys for every products
+        // and values of an array of index 2
+        // which holds the price and location
+    });
+})();
 
 // DOM Elements
-let outputEl = document.getElementById('output');
+let out = document.getElementById('output');
 
 // Main Menu & Go Button
 document.getElementById('go').addEventListener('click', mainMenu);
+
+let t = 0;
+let displayTotal = total => {
+    let totalEl = document.createElement("h3");
+    totalEl.innerHTML = `Retrieved ${total} products...`
+    out.prepend(totalEl);
+}
+let displayProduct = product => {
+    let p = document.createElement("p");
+    p.innerHTML = `<u>${product}</u><br><b>$${map[product].price}</b> from <i>${map[product].location}</i></b>`;
+    out.append(p);
+}
 
 function mainMenu() {
     // Get value of menu select element
     let selection = document.getElementById('menu').value;
 
-    // Take action based on menu selection
-    if (selection == 'option1') {
-        outputEl.innerHTML = 'You selected Option 1';
-    } else if (selection == 'option2') {
-        outputEl.innerHTML = 'You selected Option 2';
-    } else if (selection == 'option3') {
-        outputEl.innerHTML = 'You selected Option 3';
+    // Clear previous output
+    out.innerHTML = "";
+    t = 0;
+
+    // Display All
+    if(selection == 'option1') {
+        for(let product in map){
+            displayProduct(product);
+            t++;
+        }
+        displayTotal(t);
+    }
+
+    // Display Price Range
+    if(selection == 'option2') {
+        let min = Number(prompt("What is the lowest you want to pay?"));
+        let max = Number(prompt("What is the highest you want to pay?"));
+
+        if(isNaN(min) || isNaN(max) || min == "" || max == "") return alert("Invalid input.");
+
+        for(let product in map){
+            if(map[product].price > min && map[product].price < max){
+                displayProduct(product);
+                t++;
+            }
+        }
+        displayTotal(t);
+    }
+
+    // Display Country of Origin
+    if(selection == 'option3') {
+        let country = prompt("From which country?");
+
+        for(let product in map){
+            if(map[product].location.toLowerCase() == country.toLowerCase()) displayProduct(product);
+            t++;
+        }
+        displayTotal(t);
+    }
+
+    // Display Random
+    if(selection == 'option4') {
+        
+    }
+
+    // Inflation
+    if(selection == 'option5') {
+        
+    }
+
+    // Price Stats
+    if(selection == 'option6') {
+        
+    }
+
+    // Add Product
+    if(selection == 'option7') {
+        
+    }
+
+    // Remove Products
+    if(selection == 'option8') {
+        
+    }
+
+    // Remove Specific Product
+    if(selection == 'option9') {
+        
     }
 }
--- make-it-snow/script.js
+++ make-it-snow/script.js
@@ -3,14 +3,76 @@ let ctx = cnv.getContext("2d");
 cnv.width = window.innerWidth;
 cnv.height = window.innerHeight;
 
+// Scalable canvas
+window.addEventListener("resize", _ => {
+    cnv.width = window.innerWidth;
+    cnv.height = window.innerHeight;
+});
+
+// Constants
+const wind = 500;
+
+class Snowflake {
+    constructor(){
+        this.x = Math.random() * cnv.width;
+        this.y = Math.random() * cnv.height;
+        this.size = Math.random() * 3 + 1;
+        this.speed = Math.random() * 4 + 1;
+        this.shade = Math.random() * 100 + 155;
+
+        this.sparkle = Math.random() * 10;
+    }
+    update = _ => {
+        // Appearance
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, 2*Math.PI);
+
+        // Random x variation
+        this.x += (Math.random() * 2) - 1;
+
+        // Sparkle
+        if(this.shade <= 155 || this.shade >= 255) this.sparkle *= -1;
+        this.shade += this.sparkle;
+
+        // Motion
+        if(this.y < cnv.height + this.size) {
+            this.y += this.speed;
+        }else{
+            this.y = -this.size;
+            this.x = Math.random() * cnv.width;
+        }
+
+        ctx.fillStyle = `rgb(${this.shade},${this.shade},${this.shade})`;
+        ctx.fill();
+    }
+}
+
+let snowflakes = [];
 let draw = _ => {
     // Clear canvas
-    ctx.clearRect(0, 0, cnv.width, cnv.height);
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, cnv.width, cnv.height);
 
-    ctx.fillStyle = "blue";
-    ctx.fillRect(0, 0, Math.random() * 100, Math.random() * 100);
+    // Instructions
+    ctx.strokeStyle = "lightgray";
+    ctx.font = "32px times";
+    ctx.textAlign = "center";
+    ctx.strokeText(snowflakes.length > 0 ? ` ${snowflakes.length} snowflakes` : "click anywhere for snowflakes...", cnv.width/2, cnv.height/2);
+    
+    // Render snowflakes
+    for(let flake of snowflakes) flake.update();
 
+    // Next frame
     requestAnimationFrame(draw);
 }
 
 (_ => draw())();
+
+// Spawn with a click
+window.addEventListener("click", _ => {
+    if(snowflakes.length < 7000){
+        for(let i = 0; i < 50; i++){
+            snowflakes.push(new Snowflake());
+        }
+    }
+});
